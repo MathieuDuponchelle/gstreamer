6d57eb6574aad5ce22b5ff5da5ccad3578baddad
Thibault Saunier <thibault.saunier@osg.samsung.com>
2017-01-16 18:40:39 -0300

    Port to hotdoc

--- docs/hotdoc-extension/hotdoc-plugin-scanner.c
+++ docs/hotdoc-extension/hotdoc-plugin-scanner.c
@@ -1,5 +1,155 @@
 #include <gst/gst.h>
 
+static gboolean
+gtype_needs_ptr_marker (GType type)
+{
+  if (type == G_TYPE_POINTER)
+    return FALSE;
+
+  if (G_TYPE_FUNDAMENTAL (type) == G_TYPE_POINTER || G_TYPE_IS_BOXED (type)
+      || G_TYPE_IS_OBJECT (type))
+    return TRUE;
+
+  return FALSE;
+}
+
+static gboolean
+has_sometimes_template (GstElement * element)
+{
+  GstElementClass *klass = GST_ELEMENT_GET_CLASS (element);
+  GList *l;
+
+  for (l = klass->padtemplates; l != NULL; l = l->next) {
+    if (GST_PAD_TEMPLATE (l->data)->presence == GST_PAD_SOMETIMES)
+      return TRUE;
+  }
+
+  return FALSE;
+}
+
+static void
+_add_element_signals (GString * json, GstElement * element)
+{
+  gboolean opened = FALSE;
+  guint *signals;
+  guint nsignals;
+  gint i = 0, j, k;
+  GSignalQuery *query = NULL;
+  GType type;
+  GSList *found_signals, *l;
+
+  for (k = 0; k < 2; k++) {
+    found_signals = NULL;
+
+    /* For elements that have sometimes pads, also list a few useful GstElement
+     * signals. Put these first, so element-specific ones come later. */
+    if (k == 0 && has_sometimes_template (element)) {
+      query = g_new0 (GSignalQuery, 1);
+      g_signal_query (g_signal_lookup ("pad-added", GST_TYPE_ELEMENT), query);
+      found_signals = g_slist_append (found_signals, query);
+      query = g_new0 (GSignalQuery, 1);
+      g_signal_query (g_signal_lookup ("pad-removed", GST_TYPE_ELEMENT), query);
+      found_signals = g_slist_append (found_signals, query);
+      query = g_new0 (GSignalQuery, 1);
+      g_signal_query (g_signal_lookup ("no-more-pads", GST_TYPE_ELEMENT),
+          query);
+      found_signals = g_slist_append (found_signals, query);
+    }
+
+    for (type = G_OBJECT_TYPE (element); type; type = g_type_parent (type)) {
+      if (type == GST_TYPE_ELEMENT || type == GST_TYPE_OBJECT)
+        break;
+
+      if (type == GST_TYPE_BIN && G_OBJECT_TYPE (element) != GST_TYPE_BIN)
+        continue;
+
+      signals = g_signal_list_ids (type, &nsignals);
+      for (i = 0; i < nsignals; i++) {
+        query = g_new0 (GSignalQuery, 1);
+        g_signal_query (signals[i], query);
+
+        if ((k == 0 && !(query->signal_flags & G_SIGNAL_ACTION)) ||
+            (k == 1 && (query->signal_flags & G_SIGNAL_ACTION)))
+          found_signals = g_slist_append (found_signals, query);
+        else
+          g_free (query);
+      }
+      g_free (signals);
+      signals = NULL;
+    }
+
+    if (!found_signals)
+      continue;
+
+    for (l = found_signals; l; l = l->next) {
+      query = (GSignalQuery *) l->data;
+
+      g_string_append_printf (json,
+          "%s{\"name\": \"%s\","
+          "\"retval\": \"%s\","
+          "\"args\": [",
+          opened ? "," : ",\"signals\": [",
+          query->signal_name, g_type_name (query->return_type));
+
+      opened = TRUE;
+      for (j = 0; j < query->n_params; j++) {
+        g_string_append_printf (json, "%s\"%s%s\"",
+            j ? "," : "",
+            g_type_name (query->param_types[j]),
+            gtype_needs_ptr_marker (query->param_types[j]) ? " *" : "");
+      }
+      g_string_append (json, "]}");
+
+    }
+
+    g_slist_foreach (found_signals, (GFunc) g_free, NULL);
+    g_slist_free (found_signals);
+    opened = TRUE;
+  }
+
+  if (opened)
+    g_string_append (json, "]");
+}
+
+static void
+_add_element_properties (GString * json, GstElement * element)
+{
+  guint i, n_props;
+  gboolean opened = FALSE;
+  GParamSpec **specs, *spec;
+  GObjectClass *klass = G_OBJECT_GET_CLASS (element);
+
+  specs = g_object_class_list_properties (klass, &n_props);
+
+  for (i = 0; i < n_props; i++) {
+    spec = specs[i];
+
+    if (spec->owner_type != G_OBJECT_TYPE (element))
+      continue;
+    if (!opened)
+      g_string_append (json, "\"properties\": [");
+
+    g_string_append_printf (json, "%s{ \"name\": \"%s\","
+        "\"construct-only\": %s,"
+        "\"construct\": %s,"
+        "\"writable\": %s,"
+        "\"type-name\": \"%s%s\"}",
+        opened ? "," : "",
+        spec->name,
+        spec->flags & G_PARAM_CONSTRUCT_ONLY ? "true" : "false",
+        spec->flags & G_PARAM_CONSTRUCT ? "true" : "false",
+        spec->flags & G_PARAM_WRITABLE ? "true" : "false",
+        g_type_name (G_PARAM_SPEC_VALUE_TYPE (spec)),
+        gtype_needs_ptr_marker (spec->value_type) ? " *" : "");
+
+    opened = TRUE;
+  }
+
+  if (opened)
+    g_string_append (json, "]");
+
+}
+
 static void
 _add_element_details (GString * json, GstPluginFeature * feature)
 {
@@ -11,7 +161,7 @@ _add_element_details (GString * json, GstPluginFeature * feature)
       "{"
       "\"name\":\"%s\","
       "\"rank\":%d,"
-      "\"hiearchy\": [", GST_OBJECT_NAME (feature),
+      "\"hierarchy\": [", GST_OBJECT_NAME (feature),
       gst_plugin_feature_get_rank (feature));
 
   for (type = G_OBJECT_TYPE (element);; type = g_type_parent (type)) {
@@ -21,7 +171,9 @@ _add_element_details (GString * json, GstPluginFeature * feature)
     if (type == G_TYPE_OBJECT)
       break;
   }
-  g_string_append (json, "]");
+  g_string_append (json, "],");
+  _add_element_properties (json, element);
+  _add_element_signals (json, element);
 
   g_string_append (json, "}");
 }
@@ -35,9 +187,6 @@ main (int argc, char *argv[])
   GstPlugin *plugin;
   gboolean f = TRUE;
   GList *features, *tmp;
-  gint nargs = 2;
-  static gchar *args[] =
-      { "hotdoc-plugin-scanner", "--gst-disable-registry-fork", NULL };
 
   g_assert (argc == 2);
 
--- docs/hotdoc-extension/hotdoc_gst_extension/gst_extension.py
+++ docs/hotdoc-extension/hotdoc_gst_extension/gst_extension.py
@@ -1,12 +1,22 @@
 import cchardet
+import os
+import hotdoc_c_extension
+from hotdoc.core.links import Link
+from hotdoc.utils.loggable import warn, Logger
 from hotdoc.core.extension import Extension
-from hotdoc.core.symbols import ClassSymbol
+from hotdoc.core.symbols import ClassSymbol, QualifiedSymbol, PropertySymbol, SignalSymbol, ReturnItemSymbol, ParameterSymbol
 from hotdoc.parsers.gtk_doc import GtkDocParser
 from hotdoc_c_extension.c_extension import extract_comments
+from hotdoc.core.exceptions import HotdocSourceException
 from hotdoc_c_extension.c_comment_scanner.c_comment_scanner import extract_comments
-from hotdoc_c_extension.c_formatter import CFormatter
+from hotdoc_c_extension.c_formatter import CFormatter, Formatter
+from hotdoc_c_extension.gi_extension import WritableFlag, ReadableFlag, ConstructFlag, ConstructOnlyFlag
+
 import subprocess
 import json
+
+Logger.register_warning_code('signal-arguments-mismatch', HotdocSourceException,
+                             'gst-extension')
 
 SCANNER_PATH = r"@SCANNER_PATH@"
 DESCRIPTION=\
@@ -15,9 +25,25 @@ Extract gstreamer plugin documentation from sources and
 built plugins.
 """
 
+FUNDAMENTAL_TYPES = {
+    "gchararray *": "gchar *"
+}
+
 def force_unicode(data):
     encoding = cchardet.detect(data)['encoding']
     return data.decode(encoding, errors='replace')
+
+
+class GstFormatter(CFormatter):
+    def __init__(self):
+        c_extension_path = hotdoc_c_extension.__path__[0]
+        searchpath = [os.path.join(c_extension_path, "templates")]
+        Formatter.__init__(self, searchpath)
+
+    def _format_flags (self, flags):
+        template = self.engine.get_template('gi_flags.html')
+        out = template.render ({'flags': flags})
+        return out
 
 class GstExtension(Extension):
     extension_name = 'gst-extension'
@@ -30,7 +56,8 @@ class GstExtension(Extension):
         super().__init__(doc_repo)
         self.__elements = {}
         self.__raw_comment_parser = GtkDocParser(doc_repo)
-        self.formatters['html'] = CFormatter()
+        self.formatters["html"] = GstFormatter()
+        self.__pending_signals = {}
 
     def setup(self):
         stale_c, unlisted_c = self.get_stale_files(GstExtension.c_sources,
@@ -63,7 +90,13 @@ class GstExtension(Extension):
                             source_file, c[1], c[2], self.project.include_paths)
 
                         if block is not None:
+                            signal, element_name = self.__pending_signals.pop(block.name, (None, None))
+                            if signal:
+                                self.__create_signal_symbol(block.name, element_name, signal, block)
                             self.project.database.add_comment(block)
+
+        for unique_name, (signal, element_name) in self.__pending_signals.items():
+            self.__create_signal_symbol(unique_name, element_name, signal)
 
     def _get_smart_index_title(self):
         return 'GStreamer plugins documentation'
@@ -84,13 +117,87 @@ class GstExtension(Extension):
     def _get_smart_key(self, symbol):
         return symbol.extra.get('gst-element-name')
 
+    def __create_hierarchy(self, element_dict):
+        hierarchy = []
+
+        for klass_name in element_dict["hierarchy"][1:]:
+            link = Link(None, klass_name, klass_name)
+            sym = QualifiedSymbol(type_tokens=[link])
+            hierarchy.append(sym)
+
+        hierarchy.reverse()
+        return hierarchy
+
+    def __create_signal_symbol(self, unique_name, element_name, signal, block=None):
+        atypes = signal['args']
+        instance_type = unique_name.split("::")[0]
+        name = unique_name.split("::")[1]
+
+        args_type_names = []
+        if block:
+            param_names = [p for p in block.params.keys()]
+            if len(param_names) == len(atypes) + 1:
+                args_type_names = [(instance_type + " *", param_names[0])]
+                for i, _type in enumerate(atypes):
+                    args_type_names.append((_type, param_names[i + 1]))
+            else:
+                warn('signal-arguments-mismatch', "Expected arguments with types '%s', got '%s'"
+                     % (signal['args'],  param_names))
+
+        if not args_type_names:
+            args_type_names = [(instance_type + " *", 'param_0')]
+            for i in range(len(atypes)):
+                args_type_names.append((atypes[i], 'param_%s' % (i + 1)))
+
+        args_type_names.append(("gpointer", "udata"))
+        params = []
+        for _type, argname in args_type_names:
+            params.append(ParameterSymbol (argname=argname, type_tokens=_type))
+        retval = [ReturnItemSymbol (type_tokens=signal['retval'])]
+
+        self.get_or_create_symbol(SignalSymbol,
+            parameters=params, return_value=retval,
+            display_name=name, unique_name=unique_name,
+             extra={'gst-element-name': element_name})
+
+    def __create_signal_symbols(self, element):
+        for signal in element.get('signals', []):
+            self.__pending_signals["%s::%s" % (element['hierarchy'][0], signal['name'])] = signal, element['name']
+
+    def __create_property_symbols(self, element):
+        for prop in element.get('properties', []):
+            name = prop['name']
+            unique_name = '%s:%s' % (element['hierarchy'][0], name)
+            flags = [ReadableFlag()]
+            if prop['writable']:
+                flags += [WritableFlag()]
+            if prop['construct-only']:
+                flags += [ConstructOnlyFlag()]
+            elif prop['construct']:
+                flags += [ConstructFlag()]
+
+            type_name = FUNDAMENTAL_TYPES.get(prop['type-name'], prop['type-name'])
+            type_ = QualifiedSymbol (type_tokens=type_name)
+            res = self.get_or_create_symbol(PropertySymbol,
+                    prop_type=type_,
+                    display_name=name, unique_name=unique_name,
+                    extra={'gst-element-name': element['name']})
+
+            # FIXME This is incorrect, it's not yet format time (from gi_extension)
+            extra_content = self.get_formatter(self.project.output_format)._format_flags (flags)
+            res.extension_contents['Flags'] = extra_content
+
     def __parse_plugin(self, dl_path, data):
         plugin = json.loads(data.decode())
         for element in plugin.get('elements') or []:
             self.__elements[element['name']] = self.get_or_create_symbol(
                 ClassSymbol, display_name=element['name'],
-                unique_name='%s::%s' % (element['name'], element['name']),
+                hierarchy=self.__create_hierarchy(element),
+                unique_name=element['hierarchy'][0],
                 filename=dl_path, extra={'gst-element-name': element['name']})
+            self.__create_property_symbols(element)
+            self.__create_signal_symbols(element)
+
 
 def get_extension_classes():
     return [GstExtension]
